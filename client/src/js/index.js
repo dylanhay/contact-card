c0735b510a9af8cf9daddc1a6a45b0f761ae7729
dchay <dylanhay@rogers.com>
2022-10-01 11:00:06 -0700

    application configured to use the IndexedDB API

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,18 +1,21 @@
 // Import modules
 import "./form.js";
 import "./submit.js";
+import { initdb } from "./database.js";
 
 // Import CSS
-import '../css/index.css';
-import { Tooltip, Toast, Popover } from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "../css/index.css";
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Logo from '../images/logo.png';
-import Bear from '../images/bear.png';
-import Dog from '../images/dog.png';
+// Import logos
+import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
 
-window.addEventListener('load', function () {
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-  });
+window.addEventListener("load", function () {
+  initdb();
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
+});
