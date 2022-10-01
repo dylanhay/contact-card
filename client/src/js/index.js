b5846543666957d933f802e2604485cfb04300f5
dchay <dylanhay@rogers.com>
2022-09-30 19:13:03 -0700

    webpack introduced

---
+++ client/src/js/index.js
@@ -0,0 +1,12 @@
+import "./form.js";
+import "./submit.js";
+
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
+window.addEventListener('load', function () {
+    document.getElementById('logo').src = Logo;
+    document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
+  });
